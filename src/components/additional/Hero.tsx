ea77fc00d469b3c5d115680af72ca1a94d00d63e
Rennie Maharaj <rvesprey@gmail.com>
2025-02-05 21:02:00 -0400

    Adjusted hero

--- src/components/additional/Hero.tsx
+++ src/components/additional/Hero.tsx
@@ -1,5 +1,5 @@
 // components/Hero.tsx
-import { Flex } from "@radix-ui/themes";
+import { Code, Flex, Text, Tooltip } from "@radix-ui/themes";
 import React from "react";
 
 const Hero: React.FC = () => {
@@ -10,9 +10,23 @@ const Hero: React.FC = () => {
         <span className="w-full text-center">The Writer Company</span>
       </h2>
       <Flex justify={"center"} className="w-full p-4">
-        <p>
-          Explore documented content, written poetry, articles and more (soon).
-        </p>
+        <Text>
+          <Tooltip content="She shines brightly in the night sky">
+            <Code>{'const w = (s) => s.slice(1).padStart(4,"M")'}</Code>
+          </Tooltip>
+          <br />
+          Explore documented content, written poetry, articles and more{" "}
+          <Code>w(soon)</Code>.
+          <br />
+          <br />
+          <em className="italic !font-semibold animate-fade-in">
+            " But without faith it is impossible to please him:
+            <br />
+            for he that cometh to God must believe that he is,
+            <br />
+            and that he is a rewarder of them that diligently seek him. "
+          </em>
+        </Text>
       </Flex>
     </section>
   );
