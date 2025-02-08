37c789d6a6fd30aa0325bd968df7ca133e41cdf8
Rennie Maharaj <rvesprey@gmail.com>
2025-02-08 18:13:00 -0400

    Refined setup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,13 +13,13 @@ const carouselItems = [
       Explore the Bible through our <br /> AI-powered study tools
     </Text>
     <Text className="text-sm text-muted-foreground text-center mt-2"></Text>
-    <Quote className="italic animate-fade-in text-lg text-center px-6">
-      Study to shew thyself approved unto God, a workman that <br /> needeth not
-      to be ashamed, <br /> rightly dividing the word of truth.
+    <Quote className="italic animate-fade-in text-lg r px-6">
+      Study to shew thyself approved unto God, <br /> a workman that needeth not
+      to be ashamed, rightly <br /> dividing the word of truth.
     </Quote>
     <Separator size={"1"} />
-    <Quote className="italic animate-fade-in text-lg text-center px-6">
-      And take the helmet of salvation, and the sword of the Spirit, <br />{" "}
+    <Quote className="italic animate-fade-in text-lg px-6">
+      And take the helmet of salvation, and the sword of the Spirit, <br />
       which is the word of God:
     </Quote>
     <Button
@@ -36,14 +36,14 @@ const carouselItems = [
       Reinforce your faith with <br /> reasoning
     </Text>
 
-    <Quote className="italic animate-fade-in text-lg text-center px-6">
+    <Quote className="italic animate-fade-in text-lg  px-6">
       But sanctify the Lord God in your hearts,
       <br /> and be ready always to give an answer to every man <br /> who
       asketh you a reason for the hope that is in you, with <br /> meekness and
       fear.
     </Quote>
     <Separator size={"1"} />
-    <Quote className="italic animate-fade-in text-lg text-center px-6">
+    <Quote className="italic animate-fade-in text-lg  px-6">
       Above all, taking the shield of faith,
       <br /> wherewith ye shall be able to quench all the fiery darts of the
       wicked.
