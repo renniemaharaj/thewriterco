edb3aaf628ad88bd42a40d58e0a689844b504e08
Rennie Maharaj <rvesprey@gmail.com>
2025-03-08 11:48:40 -0400

    Made path file solid

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -55,7 +55,7 @@ const Page = ({
       {/* <Navbar /> */}
       <Navbar />
       <section className="p-2 w-full max-w-full">
-        <div className="flex flex-row w-full max-w-full overflow-clip p-5 gap-2 items-center ">
+        <Card className="!flex !flex-row !w-full !max-w-full !overflow-clip p-5 !gap-2 !items-center ">
           {locationParts.length > 0 && (
             <IconButton
               variant="soft"
@@ -74,7 +74,7 @@ const Page = ({
               )}
             </div>
           ))}
-        </div>
+        </Card>
       </section>
       <Ereader hidePicker={hideBiblePicker} />
       {/* <Flex className="!w-full animate-fade-in !flex-col merriweather-bold !p-2"> */}
