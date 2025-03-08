ff8b74582c0e690efedda5ce1c01ddf23fbf2b7f
Rennie Maharaj <rvesprey@gmail.com>
2025-03-07 22:19:20 -0400

    Adjusted input

--- src/components/ai/Input.tsx
+++ src/components/ai/Input.tsx
@@ -82,11 +82,10 @@ const Input: React.FC<InputProps> = ({
       style={style}
       ref={wrapperRef}
       className={`${className} ${
-        inputFocus
-          ? "border-[#978365] border-2"
-          : `border-transparent !max-w-[64px] !max-h-[64px] !w-[64px] !h-[64px] w-aspect-square !opacity-40 ${chatSlice.viewMode === "canvas" ? "ml-12" : ""}`
-      } w-[100%] outline-none !flex !flex-col !gap-2
-       `}
+        inputFocus && "border-[#978365] border-2 w-[100%]"
+        // : `border-transparent !max-w-[64px] !max-h-[64px] !w-[64px] !h-[64px] aspect-square !opacity-40 `
+      } ${!inputFocus && "!w-[64px] !h-[64px]"} outline-none !flex !flex-col !gap-2
+       ${chatSlice.viewMode === "canvas" ? "ml-12 mb-[1rem]" : ""}`}
     >
       {!inputFocus && (
         <Flex
