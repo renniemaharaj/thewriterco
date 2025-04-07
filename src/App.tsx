3f56b47a5ae3dd3740734f77f20e8348db20dd88
Rennie Maharaj <rvesprey@gmail.com>
2025-04-07 13:29:14 -0400

    Adjusted App.tsx implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,54 @@
+import { Routes, Route } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
-import { Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { Theme } from "@radix-ui/themes";
 
-import { HelmetProvider } from "react-helmet-async";
-
-// Import components
 import PersistLogin from "./components/PersistLogin";
 import ErrorFallback from "./components/ErrorBoundary";
 import { RequireAuth } from "./components/RequireAuth";
 import { ThemeProvider } from "./components/context/theme/ThemeProvider";
 import { useThemeContext } from "./components/context/theme/useThemeContext";
-
-// Import pages
-import { lazy } from "react";
-import Root from "./pages/root/Root";
-import NoPage from "./pages/noPage/NoPage";
-import Study from "./pages/reasoning/Reasoning";
-
-// Lazy loaded components
-const AI = lazy(() => import("./pages/ai/Chat"));
-const Number = lazy(() => import("./pages/number/Number"));
-const Guide = lazy(() => import("./pages/doc/studyDocument/Guide"));
-const GeminiPool = lazy(() => import("./pages/doc/geminiPool/Guide"));
-const Kjv = lazy(() => import("./pages/kjv/KJV"));
-
-// Custom route types
-type CustomRoute = {
-  path: string;
-  element: JSX.Element;
-};
-
-// Index route type
-type IndexRoute = {
-  index: true;
-  element: JSX.Element;
-};
-
-// List of public routes
-const publicRoutes: (CustomRoute | IndexRoute)[] = [
-  {
-    index: true,
-    element: <Root />,
-  },
-  {
-    path: "*",
-    element: <NoPage />,
-  },
-  {
-    path: "deducer",
-    element: <Number />,
-  },
-  {
-    path: "ai",
-    element: <AI />,
-  },
-  {
-    path: "kjv",
-    element: <Kjv />,
-  },
-  {
-    path: "reasoning",
-    element: <Study />,
-  },
-  {
-    path: "doc/studyDocument",
-    element: <Guide />,
-  },
-  {
-    path: "doc/geminiPool",
-    element: <GeminiPool />,
-  },
-];
-
-// List of protected routes
-const protectedRoutes: CustomRoute[] = [];
+import { protectedRoutesFunc, publicRoutesFunc } from "./twcConfig";
 
 function App() {
   return (
     <ThemeProvider>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <AppContent />
+        <AppShell />
       </ErrorBoundary>
     </ThemeProvider>
   );
 }
 
-function AppContent() {
+function AppShell() {
   const { theme } = useThemeContext();
 
   return (
     <Theme
       appearance={theme}
-      accentColor={"gold"}
+      accentColor="gold"
       radius="full"
       panelBackground="translucent"
       scaling="110%"
       grayColor="sage"
     >
       <HelmetProvider>
-        <Routes>
-          {/* Public Routes */}
-          {publicRoutes.map((route, index) =>
-            "index" in route ? (
-              <Route key={"pub-route-" + index} index element={route.element} />
-            ) : (
-              <Route
-                key={"pub-route-" + index}
-                path={route.path}
-                element={route.element}
-              />
-            ),
-          )}
-
-          {/* Protected Routes and Login Persistent Routes */}
-          <Route element={<PersistLogin />}>
-            <Route element={<RequireAuth />}>
-              {protectedRoutes.map((route, index) => (
-                <Route
-                  key={"priv-route-" + index}
-                  path={route.path}
-                  element={route.element}
-                />
-              ))}
-            </Route>
-          </Route>
-        </Routes>
+        <AppRoutes />
       </HelmetProvider>
     </Theme>
+  );
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Public Routes */}
+      {publicRoutesFunc()}
+      {/* Protected Routes */}
+      <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth />}>{protectedRoutesFunc()}</Route>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/twcConfig.tsx
@@ -0,0 +1,61 @@
+import { Route } from "react-router-dom";
+
+import { lazy } from "react";
+import Root from "./pages/root/Root";
+import NoPage from "./pages/noPage/NoPage";
+import Study from "./pages/reasoning/Reasoning";
+
+const AI = lazy(() => import("./pages/ai/Chat"));
+const Number = lazy(() => import("./pages/number/Number"));
+const Guide = lazy(() => import("./pages/doc/studyDocument/Guide"));
+const GeminiPool = lazy(() => import("./pages/doc/geminiPool/Guide"));
+const Kjv = lazy(() => import("./pages/kjv/KJV"));
+
+export interface Primitve {
+  element: JSX.Element;
+}
+
+export interface CustomRoute extends Primitve {
+  path: string;
+}
+
+export interface IndexRoute extends Primitve {
+  index: true;
+}
+
+export const publicRoutes: (CustomRoute | IndexRoute)[] = [
+  { index: true, element: <Root /> },
+  { path: "*", element: <NoPage /> },
+  { path: "deducer", element: <Number /> },
+  { path: "ai", element: <AI /> },
+  { path: "kjv", element: <Kjv /> },
+  { path: "reasoning", element: <Study /> },
+  { path: "doc/studyDocument", element: <Guide /> },
+  { path: "doc/geminiPool", element: <GeminiPool /> },
+];
+
+const protectedRoutes: CustomRoute[] = [{ path: "office", element: <></> }];
+
+export const publicRoutesFunc = () => {
+  return publicRoutes.map((route, i) =>
+    "index" in route ? (
+      <Route key={`public-index` + i} index element={route.element} />
+    ) : (
+      <Route
+        key={`public-${route.path}` + i}
+        path={route.path}
+        element={route.element}
+      />
+    ),
+  );
+};
+
+export const protectedRoutesFunc = () => {
+  return protectedRoutes.map((route, i) => (
+    <Route
+      key={`private-${route.path}` + i}
+      path={route.path}
+      element={route.element}
+    />
+  ));
+};
