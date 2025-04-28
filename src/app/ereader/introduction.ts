b21f45dd22fcabb099e88745a8b514d468465b07
Rennie Maharaj <rvesprey@gmail.com>
2025-04-28 00:30:49 -0400

    Disabled introduction message

--- src/app/ereader/introduction.ts
+++ src/app/ereader/introduction.ts
@@ -1,8 +1,2 @@
-const introductionMessage = `
-Welcome to TheWriterCo.
-
-Explore the King James Version of the Holy Bible, the complete and authoritative Word of God. 
-
-We offer it for online reading, writing and as a modern JSON format on GitHub for developers. Begin your journey into scripture today!
-`;
+const introductionMessage = ``;
 export default introductionMessage;
