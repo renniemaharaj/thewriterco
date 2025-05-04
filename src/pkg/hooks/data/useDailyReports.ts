307f5eb04eb906fcc904a26a69760e7c9b749b7c
Rennie Maharaj <rvesprey@gmail.com>
2025-05-04 06:12:53 -0400

    Customized daily report params

--- src/pkg/hooks/data/useDailyReports.ts
+++ src/pkg/hooks/data/useDailyReports.ts
@@ -10,7 +10,19 @@ export type ReportObject = {
   images?: string[];
 };
 
-export function useDailyReports() {
+export type UseDailyReportsParams = {
+  max?: number;
+  index?: number;
+  q?: string;
+  relevance?: number;
+};
+
+export function useDailyReports({
+  max = 30,
+  index,
+  q,
+  relevance,
+}: UseDailyReportsParams = {}) {
   const [reports, setReports] = useState<ReportObject[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -18,9 +30,15 @@ export function useDailyReports() {
   useEffect(() => {
     const fetchReports = async () => {
       try {
-        // const baseServer = "http://localhost:4000/reports";
+        const params = new URLSearchParams();
+        if (q) params.append("q", q);
+        if (typeof index === "number") params.append("index", index.toString());
+        if (typeof max === "number") params.append("max", max.toString());
+        if (typeof relevance === "number")
+          params.append("relevance", relevance.toString());
+
         const baseServer = "https://daily-reports.onrender.com/reports";
-        const res = await fetch(baseServer);
+        const res = await fetch(`${baseServer}?${params.toString()}`);
         if (!res.ok) {
           throw new Error(`HTTP error: ${res.status}`);
         }
@@ -34,7 +52,7 @@ export function useDailyReports() {
     };
 
     fetchReports();
-  }, []);
+  }, [q, index, max, relevance]);
 
   return { reports, loading, error };
 }
