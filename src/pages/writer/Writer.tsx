58082ad24eebaccd633947a862c3f7cd3bd83091
Rennie Maharaj <rvesprey@gmail.com>
2025-06-13 14:31:32 -0400

    Corrected writer

--- src/pages/writer/Writer.tsx
+++ src/pages/writer/Writer.tsx
@@ -1,23 +1,28 @@
-import { useDispatch, useSelector } from "react-redux";
+// import {
+// useDispatch,
+//  useSelector } from "react-redux";
 import Page from "../../pkg/page/Page";
 import Editor from "../../pkg/writer/Editor";
-import { RootState } from "../../app/store";
-import { useCallback } from "react";
-import { setContent } from "../../app/writer/writerSlice";
+// import { RootState } from "../../app/store";
+// import { useCallback } from "react";
+// import { setContent } from "../../app/writer/writerSlice";
 
 const Writer = () => {
-  const writer = useSelector((state: RootState) => state.writer);
-  const content = writer.content;
-  const dispatch = useDispatch();
+  // const writer = useSelector((state: RootState) => state.writer);
+  // const content = writer.content;
+  // const dispatch = useDispatch();
 
-  const setContentCallback = useCallback(
-    (content: string) => dispatch(setContent(content)),
-    [dispatch],
-  );
+  // const setContentCallback = useCallback(
+  //   (content: string) => dispatch(setContent(content)),
+  //   [dispatch],
+  // );
 
   return (
     <Page wrapChildren title="Writer" description="Online Writer">
-      <Editor content={content} setContent={setContentCallback} />
+      <Editor
+      //  content={content}
+      //  setContent={setContentCallback}
+      />
     </Page>
   );
 };
