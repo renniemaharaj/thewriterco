0fa647e89883dd8c1e37198593707c6242167f15
Rennie Maharaj <rvesprey@gmail.com>
2025-06-29 12:23:11 -0400

    Added reasonable max delay

--- src/pkg/docs/cmpnts/Menu.tsx
+++ src/pkg/docs/cmpnts/Menu.tsx
@@ -79,7 +79,7 @@ const Menu = ({ additionalTabs = [], className }: MenuProps) => {
           key={index + "tabsItem"}
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3, delay: index * 0.2 }}
+          transition={{ duration: 0.3, delay: min([index * 0.2, 0.8]) }}
         >
           <Item urlTab={tab.value} title={item.title} />
         </motion.div>
