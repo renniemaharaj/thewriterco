6bc3fb5b672f67fe1543dc59a2b19151f1e024bc
Rennie Maharaj <rvesprey@gmail.com>
2025-07-08 19:58:10 -0400

    Simplified auth

--- src/pkg/firebase/auth/component/Auth.tsx
+++ src/pkg/firebase/auth/component/Auth.tsx
@@ -1,18 +1,15 @@
-import { useEffect, useCallback, useMemo } from "react";
+import { useCallback } from "react";
 import { GoogleAuthProvider, signOut, signInWithPopup } from "firebase/auth";
 
-import { useDispatch } from "react-redux";
 import { Button } from "@radix-ui/themes";
-import { AuthResult } from "../../../../app/api/auth/authTypes";
-import { setCredentials } from "../../../../app/api/auth/authSlice";
 import { auth } from "../../firebase";
 import Card from "./user/Card";
 import useUserLikelySignedIn from "../hooks/useUserLikelySignedIn";
+import useAuthResultSyncer from "../hooks/useAuthResultSyncer";
 
 const Auth = ({ variant = "button" }: { variant?: "image" | "button" }) => {
-  const { user, token } = useUserLikelySignedIn();
-
-  const dispatch = useDispatch();
+  const { user } = useUserLikelySignedIn();
+  const syncAuthResult = useAuthResultSyncer();
 
   const handleSignIn = useCallback(async () => {
     try {
@@ -26,41 +23,11 @@ const Auth = ({ variant = "button" }: { variant?: "image" | "button" }) => {
   const handleSignOut = async () => {
     try {
       await signOut(auth);
-      dispatch(setCredentials(noAuthResult));
+      syncAuthResult(null);
     } catch (err) {
       console.error("Logout failed", err);
     }
   };
-
-  const noAuthResult = useMemo<AuthResult>(
-    () => ({
-      user: null,
-      accessToken: null,
-      message: "User signed out",
-    }),
-    [],
-  );
-
-  useEffect(() => {
-    if (user) {
-      const parts = user.displayName?.split(" ");
-      const fname = parts?.[0] || "";
-      const lname = parts?.slice(1).join(" ") || "";
-      const authResult: AuthResult = {
-        user: {
-          _id: user.uid,
-          userName: user.displayName || "",
-          firstName: fname,
-          lastName: lname,
-          emailAddress: user.email || "",
-        },
-        accessToken: token,
-      };
-      dispatch(setCredentials(authResult));
-    } else {
-      dispatch(setCredentials(noAuthResult));
-    }
-  }, [user, token, dispatch, noAuthResult]);
 
   return variant === "image" ? (
     <Card
