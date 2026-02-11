98a6a63b7047ae44f4bd27929e9b5c0815e2f834
renniemaharaj <rvesprey@gmail.com>
2026-02-10 20:42:26 -0400

    made kjv public

--- src/routes.tsx
+++ src/routes.tsx
@@ -16,24 +16,24 @@ import Login from "./pages/login/index";
 export const protectedRoutes: (CustomRoute | IndexRoute)[] = [
   { path: "*", element: <Missing /> },
   { path: "login", element: <Login /> },
-  { path: "kjv", element: <Kjv /> },
-  { path: "kjv/:title/:chapter/:verse", element: <Kjv /> },
   { path: "read", element: <Documentation /> },
   { path: "read/:tab", element: <Documentation /> },
   { path: "read/:tab/:title", element: <Documentation /> },
   { path: "daily", element: <Daily /> },
   { path: "daily/:searchQuery/:resultTitle", element: <Daily /> },
-
+  
   // Depreciated
   { path: "reasoning/:tab/:title", element: <Documentation /> },
   { path: "ai", element: <AI />, suspended: false },
   { path: "office", element: <></> },
   { path: "studies", element: <></> },
-
+  
   { path: "deduce", element: <Number /> },
   // { path: "search/:searchQuery", element: <Daily /> },
 ];
 export const publicRoutes: (CustomRoute | IndexRoute)[] = [
   { index: true, element: <Index /> },
+  { path: "kjv", element: <Kjv /> },
+  { path: "kjv/:title/:chapter/:verse", element: <Kjv /> },
   { path: "writer", element: <Writer />, suspended: false },
 ];
